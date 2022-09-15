ff67f9f91290869d831af55c6ef62f55eb12ded2
Hasanoruc466 <63128418+Hasanoruc466@users.noreply.github.com>
2022-09-15 20:04:57 +0300

    last-commit

--- TheMovieManager/app/src/main/java/com/example/themoviemanager/APIUtils.java
+++
@@ -1,8 +0,0 @@
-package com.example.themoviemanager;
-
-public class APIUtils {
-    private static String baseUrl = "https://api.themoviedb.org";
-    public static APIDao getSearch(){
-        return RetrofitClient.getRetrofit(baseUrl).create(APIDao.class);
-    }
-}
